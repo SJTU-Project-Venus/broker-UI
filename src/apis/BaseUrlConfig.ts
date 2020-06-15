f0c0939432b3ee94b845cd86f577e6cae482a7a3
wzy <2596409301@qq.com>
2020-06-15 22:41:22 +0800

    bugfix

--- src/apis/BaseUrlConfig.ts
+++ src/apis/BaseUrlConfig.ts
@@ -1,4 +1,1 @@
-export const TRADER_BACKEND = 'http://trader-gateway.traderone:8080'; //'http://localhost:8081';
-export const BROKER_GATEWAY = 'http://broker-gateway.brokerone:8080'; //'http://localhost:8080';
-export const TRADER_WEBSOCKET = 'ws://trader-gateway.traderone:8080/ws'; //'ws://localhost:8081/ws';
 export const Broker_WEBSOCKET = "ws://202.120.40.8:30386/two/websocket/broker-ui";
