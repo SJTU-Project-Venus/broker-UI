78f193ba8c55d25952d62aeb69042993d18b7454
SAIL <1127858301@qq.com>
2020-06-15 18:11:54 +0800

    broker otwo

--- src/apis/BaseUrlConfig.ts
+++ src/apis/BaseUrlConfig.ts
@@ -1,4 +1,4 @@
 export const TRADER_BACKEND = 'http://trader-gateway.traderone:8080'; //'http://localhost:8081';
 export const BROKER_GATEWAY = 'http://broker-gateway.brokerone:8080'; //'http://localhost:8080';
 export const TRADER_WEBSOCKET = 'ws://trader-gateway.traderone:8080/ws'; //'ws://localhost:8081/ws';
-export const Broker_WEBSOCKET = "ws://202.120.40.8:30382/two/websocket/broker-ui";
+export const Broker_WEBSOCKET = "ws://202.120.40.8:30382/one/websocket/broker-ui";
